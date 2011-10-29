62c336e1208ee70d9d65efa8a530361117ba0b9c
Mark Roghelia <mroghelia@topsailtech.com>
2011-10-29 10:48:20 -0400

    Generated new Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LivingSocial::Application.config.secret_token = '16c00ed1cfad21d3f5572668caaa64e8d417fc01c73b7254bcfc9d91b1d3d9b8d4284cdb04447e9fe685ba8aed8959e210f87dff52d3f883a8494a7dd066b404'
